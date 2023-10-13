bde0f5f7b0331e573f74b0fd8b53201e49849f12
Cody Hager <cghager@umich.edu>
2023-10-13 15:13:39 -0400

    Day 2 work UM carp workshop

--- code/gdp_population.r
+++ code/gdp_population.r
@@ -1,7 +1,8 @@
 library(tidyverse) #loading tidyveres package
 
 #the next line reads in the gapminder_1997.csv flie
-gapminder_1997 <- read_csv(file = "data/gapminder_1997.csv")
+gapminder_1997 <- read_csv(file = "Data/gapminder_1997.csv")
+
 
 #this assigns a argument value to the name function
 name <- "Burness Lab"
@@ -19,7 +20,7 @@ round(digits=2, x=3.1415)
 ################## plotting ##########################
 #using ggplot2
 
-ggplot(data=gapminder_1997)
+ggplot(Sata=gapminder_1997)
 
 #need to tell ggplot astetic mapping. this tells it what we want to map on x and y and what color and size
 #need to add a layer, the + then enter tabs over the line and includes it in the code line above
@@ -27,7 +28,7 @@ ggplot(data=gapminder_1997)
 #labs changes labels
 # aes(color= ) can color biased on parameter. To spesify color use  scale_color_brewer
 
-ggplot(data=gapminder_1997) +
+ggplot(gapminder_1997) +
   aes(x=gdpPercap) +
   labs(x="GDP Per Capita")+
   aes(y=lifeExp)+
@@ -39,20 +40,15 @@ ggplot(data=gapminder_1997) +
   aes(size=pop/1000000)+
   labs(size="Population (Millions)")+
   aes(color=continent)
+ggsave("figures/gdpPercap_lifeExp.png")
+  
 
 
 #consolidate the above code
 
-ggplot(data=gapminder_1997) +
-  aes(x=gdpPercap, 
-      y=lifeExp,
-      shape=continent,
-      size=pop/1000000,
-      color=continent) +
+ggplot(gapminder_1997) +
+  aes(x=gdpPercap, y=lifeExp, shape=continent,size=pop/1000000, color=continent) +
   geom_point()+
-  labs(size="Population (Millions)",
-       title= "Do people in wealthy countries live longer?",
-       x="GDP Per Capita",
-       y="Life Expectancy")+
-  scale_color_brewer(palette="Dark2")
+  labs(size="Population (Millions)", title= "Do people in wealthy countries live longer?", x="GDP Per Capita", y="Life Expectancy")+
+  scale_color_brewer(palette="Dark2")+ ggsave("figures/gdpPercap_lifeExp.png")
 
